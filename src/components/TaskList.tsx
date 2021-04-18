5a1afa94a6da25d46405a546201fc3d284bf5fb3
Rafael Ferreira <ffael@live.com>
2021-04-18 01:14:08 -0400

    Adds missing features to complete the challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -12,18 +12,54 @@ interface Task {
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [error, setError] = useState(false)
   const [newTaskTitle, setNewTaskTitle] = useState('');
+
+  useEffect(() => {
+    setError(false)
+  }, [newTaskTitle])
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === '') {
+      return setError(true)
+    }
+
+    const duplicateTitle = tasks.find((task) => task.title === newTaskTitle)
+
+    if (duplicateTitle) {
+      const confirm = window.confirm('Ja existe uma tarefa com esse titulo, deseja criar uma nova tarefa com o mesmo titulo?')
+
+      if (!confirm) {
+        return
+      }
+    }
+    setTasks([...tasks, {
+      id: Math.floor(Math.random() * 100),
+      title: newTaskTitle,
+      isComplete: false
+    }])
+    setNewTaskTitle('')
+    setError(false)
   }
+
+
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let found = tasks.find((task) => task.id === id)
+    if (found) {
+      found.isComplete = !found.isComplete
+      setTasks([...tasks])
+    }
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    let remainder = tasks.filter((task) => task.id !== id)
+    if (remainder) {
+      setTasks([...remainder])
+    }
   }
 
   return (
@@ -32,25 +68,28 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          {error && <strong style={{ color: 'red', fontSize: '12px', userSelect: 'none' }}>A tarefa precisa de um titúlo!</strong>}
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+            <FiCheckSquare size={16} color="#fff" />
           </button>
+
         </div>
       </header>
 
       <main>
+        {tasks.length === 0 && <h3 style={{ width: '100%', textAlign: 'center', color: '#ccc', userSelect: 'none' }}>Nenhuma Tarefa Adicionada!</h3>}
         <ul>
           {tasks.map(task => (
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -62,11 +101,11 @@ export function TaskList() {
               </div>
 
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
